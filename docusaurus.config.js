8cf1b896dca182c69d3b31973fac6ee17c528441
Jochen Zehnder <jochen.zehnder@gmail.com>
2022-07-22 14:55:02 +0200

    removed blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,13 +40,6 @@ const config = {
           editUrl:
             'https://github.com/elft3r/devops-training/tree/main/'
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/elft3r/devops-training/tree/main/'
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
         }
@@ -70,7 +63,6 @@ const config = {
             position: 'left',
             label: 'Tutorial'
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/elft3r/devops-training',
             label: 'GitHub',
@@ -106,10 +98,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog'
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/elft3r/devops-training'
